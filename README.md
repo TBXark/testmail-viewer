b2cf06e0735d07e35cac0f7b22178f4873b25553
TBXark <tbxark@outlook.com>
2022-02-16 13:51:38 +0800

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# TestMail Viewer
+
+TestMail 免费版不支持在线预览邮件，只能支持在线预览json，所以使用起来有点麻烦，所以就有了这个在线预览TestMail的工具。
+使用下面的链接就可以直接预览邮件，暂时不支持富文本。
+
+```
+https://testmail-viewer.tbxark.workers.dev/?apikey=#YOUR_API_KEY&namespace=#YOUR_NAME_SPACE
+
+```
---
+++ static/html.js
@@ -0,0 +1,32 @@
+export default `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="" />
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
+  <meta name="generator" content="Hugo 0.88.1" />
+  <title>Mail</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="index.css" rel="stylesheet" />
+</head>
+<body>
+  <main>
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 500px">
+      <a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+        <span class="fs-5 fw-semibold">Temp Mail</span>
+      </a>
+      <div class="list-group list-group-flush border-bottom scrollarea">
+        <!-- container -->
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div id="mail-container"></div>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+  <script src="index.js"></script>
+</body>
+</html>
+`
