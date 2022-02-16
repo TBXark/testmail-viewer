b0c83d122c5afef5f8e3c131de644f2ee45971c4
TBXark <tbxark@outlook.com>
2022-02-16 16:14:59 +0800

    添加富文本支持

--- static/css.js
+++ static/css.js
@@ -1,46 +1,46 @@
 export default `
 body {
-    min-height: 100vh;
-    min-height: -webkit-fill-available;
-  }
-  
-  html {
-    height: -webkit-fill-available;
-  }
-  
-  main {
-    display: flex;
-    flex-wrap: nowrap;
-    height: 100vh;
-    height: -webkit-fill-available;
-    max-height: 100vh;
-    overflow-x: auto;
-    overflow-y: hidden;
-  }
-  
-  .divider {
-    flex-shrink: 0;
-    width: 2px;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, .1);
-    border: solid rgba(0, 0, 0, .15);
-    border-width: 1px 0;
-  }
-  
-  .dropdown-toggle { outline: 0; }
-  
-  .nav-flush .nav-link {
-    border-radius: 0;
-  }
-  
-  .scrollarea {
-    overflow-y: auto;
-  }
-  
-  .fw-semibold { font-weight: 600; }
-  .lh-tight { line-height: 1.25; }
-  
-  #mail-container {
-    padding: 20px
-  }
+  min-height: 100vh;
+  min-height: -webkit-fill-available;
+}
+
+html {
+  height: -webkit-fill-available;
+}
+
+main {
+  display: flex;
+  flex-wrap: nowrap;
+  height: 100vh;
+  height: -webkit-fill-available;
+  max-height: 100vh;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+
+.divider {
+  flex-shrink: 0;
+  width: 2px;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, .1);
+  border: solid rgba(0, 0, 0, .15);
+  border-width: 1px 0;
+}
+
+.dropdown-toggle { outline: 0; }
+
+.nav-flush .nav-link {
+  border-radius: 0;
+}
+
+.scrollarea {
+  overflow-y: auto;
+}
+
+.fw-semibold { font-weight: 600; }
+.lh-tight { line-height: 1.25; }
+
+#iframe-container {
+  padding: 20px;
+}
 `
--- static/html.js
+++ static/html.js
@@ -8,22 +8,22 @@ export default `
   <meta name="description" content="" />
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
   <meta name="generator" content="Hugo 0.88.1" />
-  <title>Test Mail</title>
+  <title>TestMail</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link href="index.css" rel="stylesheet" />
 </head>
 <body>
   <main>
-    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 500px">
+    <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 450px">
       <a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-        <span class="fs-5 fw-semibold">Test Mail</span>
+        <span class="fs-5 fw-semibold">TestMail</span>
       </a>
       <div class="list-group list-group-flush border-bottom scrollarea">
         <!-- container -->
       </div>
     </div>
     <div class="divider"></div>
-    <div id="mail-container"></div>
+    <div id="mail-container" style="width: 100%"></div>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
   <script src="index.js"></script>
