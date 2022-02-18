ef52e7a20aba6901d698ff9869d167d392e933de
TBXark <tbxark@outlook.com>
2022-02-18 16:46:07 +0800

    add error handler

--- functions/api/emails.js
+++ functions/api/emails.js
@@ -1,8 +1,12 @@
 export async function onRequestGet(request) {
-  let url = new URL(request.url)
-  let token = url.searchParams.get('apikey')
-  let namespace = url.searchParams.get('namespace')
-  return await fetch(
-    `https://api.testmail.app/api/json?apikey=${token}&namespace=${namespace}&pretty=true`,
-  )
+  try {
+    let url = new URL(request.url)
+    let token = url.searchParams.get('apikey')
+    let namespace = url.searchParams.get('namespace')
+    return await fetch(
+      `https://api.testmail.app/api/json?apikey=${token}&namespace=${namespace}&pretty=true`,
+    )
+  } catch (error) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 })
+  }
 }
