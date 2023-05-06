a075fc477164435c9a8979de110c29ac45e090ce
TBXark <tbxark@outlook.com>
2023-05-06 10:51:37 +0800

    fix: request bug

--- functions/api/emails.js
+++ functions/api/emails.js
@@ -1,9 +1,8 @@
 export async function onRequest(context) {
   try {
-    const url = new URL(context.request.url)
     const { apikey, namespace } = await context.request.json()
     return await fetch(
-      `https://api.testmail.app/api/json?apikey=${token}&namespace=${namespace}&pretty=true`,
+      `https://api.testmail.app/api/json?apikey=${apikey}&namespace=${namespace}&pretty=true`,
     )
   } catch (error) {
     return new Response(JSON.stringify({
