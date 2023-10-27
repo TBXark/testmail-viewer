badc802a31be774d5734e5a881c8dda42cd93808
TBXark <tbxark@outlook.com>
2023-10-27 13:39:01 +0800

    feat: 添加翻页功能

--- functions/api/emails.js
+++ functions/api/emails.js
@@ -1,9 +1,9 @@
 export async function onRequest(context) {
   try {
     const raw = await context.request.text()
-    const { apikey, namespace } = JSON.parse(atob(raw))
+    const { apikey, namespace, offset, limit } = JSON.parse(atob(raw))
     return await fetch(
-      `https://api.testmail.app/api/json?apikey=${apikey}&namespace=${namespace}&pretty=true`,
+      `https://api.testmail.app/api/json?apikey=${apikey}&namespace=${namespace}&pretty=true&offset=${offset || 0 }&limit=${limit || 10}`,
     )
   } catch (error) {
     return new Response(JSON.stringify({
